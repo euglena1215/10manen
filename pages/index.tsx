464790454756d7682b94a602cbee08c7916117f3
Teppei Shintani <teppest1215@gmail.com>
2020-05-03 16:15:17 +0900

    style index

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,29 @@ import Layout from "../components/Layout";
 export default () => (
   <Layout>
     <Title>10万円支給されるやつ（仮）</Title>
+    <ImgWrapper>
+      <img src="/images/index.png" alt="" />
+    </ImgWrapper>
 
-    <Link href="/Intro">
-      <a>スタート</a>
-    </Link>
+    <StartLink>
+      <Link href="/Intro">
+        <a>スタート</a>
+      </Link>
+    </StartLink>
   </Layout>
 );
 
 const Title = styled.h1`
   font-size: 2.5rem;
   text-align: center;
+`;
+
+const ImgWrapper = styled.div`
+  text-align: center;
+`;
+
+const StartLink = styled.div`
+  margin: 20px;
+  text-align: center;
+  font-size: 1.5rem;
 `;

a551056b75b098c0265d9d7aae4e2ff9a1e95468
Teppei Shintani <teppest1215@gmail.com>
2020-05-03 15:05:08 +0900

    fix

--- pages/Result.tsx
+++ pages/Result.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
+import styled from "styled-components";
 
-export default ({ query }) => {
+export default () => {
   const router = useRouter();
   const { consumeRate, userVoices } = router.query;
 
@@ -13,17 +14,43 @@ export default ({ query }) => {
 
   return (
     <Layout>
-      結果画面
-      <p>想定財源{zaigen}億円に対して...</p>
-      <p>
-        {(zaigen * Number(consumeRate)).toString().slice(0, 3)}億円使いました！
-      </p>
-      <p>国民の声</p>
-      {formattedUserVoices &&
-        formattedUserVoices.map((userVoice) => <p>{userVoice}</p>)}
+      <Title>
+        <p>想定財源{zaigen}億円に対して...</p>
+        <p>
+          {(zaigen * Number(consumeRate)).toString().slice(0, 3)}
+          億円使いました！
+        </p>
+      </Title>
+      <UserVoice>
+        <UserVoiceTitle>国民の声</UserVoiceTitle>
+
+        {formattedUserVoices &&
+          formattedUserVoices.map((userVoice) => (
+            <UserVoiceContent>
+              <p>@xxxx: {userVoice}</p>
+            </UserVoiceContent>
+          ))}
+      </UserVoice>
+
       <Link href="/CreateForm">
         <a>作りなおす</a>
       </Link>
     </Layout>
   );
 };
+
+const Title = styled.div`
+  margin: 20px;
+  font-size: 2rem;
+  text-align: center;
+`;
+
+const UserVoice = styled.div``;
+
+const UserVoiceTitle = styled.p`
+  font-size: 1.5rem;
+`;
+
+const UserVoiceContent = styled.div`
+  margin: 10px;
+`;
